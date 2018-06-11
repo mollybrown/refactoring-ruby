1264c802b2e22a1d76fefbd995d1f4c9379382c1
Molly Brown <mollyrachelbrown@gmail.com>
2018-06-10 17:46:19 -0700

    ch1: move `frequent_rental_points` method to Movie class as points calculation also varies with movie type

--- ch1/rental.rb
+++ ch1/rental.rb
@@ -11,7 +11,7 @@ class Rental
   end
 
   def frequent_renter_points
-    (movie.price_code == Movie::NEW_RELEASE && rental.days_rented > 1) ? 2 : 1
+    movie.frequent_renter_points(days_rented)
   end
 
 end
