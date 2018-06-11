08f36567f68689b47970a50659d17703b2e42150
Molly Brown <mollyrachelbrown@gmail.com>
2018-06-10 17:21:56 -0700

    ch1: Add `html_statment` method

--- ch1/customer.rb
+++ ch1/customer.rb
@@ -12,7 +12,6 @@ class Customer
 
   def statement
     result = "Rental Record for #{@name}\n"
-
     @rentals.each do |rental|
       frequent_renter_points += rental.frequent_renter_points
       # show figures for this rental
@@ -22,6 +21,20 @@ class Customer
     # add footer lines
     result += "Amount owed is #{total_charge}\n"
     result += "You earned #{total_frequent_renter_points} frequent renter points"
+    result
+  end
+
+  def html_statement
+    result = "<h1>Rentals for <em>#{@name}</em></h1><p>\n"
+    @rentals.each do |rental|
+      # show figures for this rental
+      result += "\t" + rental.movie.title + ": " + rental.charge.to_s + "<br>\n"
+    end
+    # add footer lines
+    result += "<p>Amount owed is <em>#{total_charge}</em><p>\n"
+    result += "You earned " +
+              "<em>#{total_frequent_renter_points}</em> " +
+              "frequent renter points<p>"
     result
   end
 
