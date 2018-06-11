e1b182b4f0537a48bd6ddaab97a2dde9e0d81c51
Molly Brown <mollyrachelbrown@gmail.com>
2018-06-10 18:30:55 -0700

    ch1: refactor Movie#charge to delegate price calculation to new movie type subclasses

--- ch1/movie.rb
+++ ch1/movie.rb
@@ -8,6 +8,11 @@ class Movie
 
   def price_code(value)
     @price_code = value
+    @price = case price_code
+      when REGULAR: RegularPrice.new
+      when NEW_RELEASE: NewReleasePrice.new
+      when CHILDRENS: ChildrensPrice.new
+    end
   end
 
   def initialize(title, the_price_code)
@@ -16,19 +21,7 @@ class Movie
   end
 
   def charge(days_rented)
-    result = 0
-
-    case price_code
-    when Movie::REGULAR
-      result += 2
-      result += (days_rented - 2) * 1.5 if days_rented > 2
-    when Movie::NEW_RELEASE
-      result += days_rented * 3
-    when Movie::CHILDRENS
-      result += 1.5
-      result += (days_rented - 3) * 1.5 if days_rented > 3
-    end
-    result
+    @price.charge(days_rented)
   end
 
   def frequent_renter_points(days_rented)
